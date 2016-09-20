6619367c884df71e89d6b0ce982e7f3674db6a43
Agust Ingi kjartansson <agustik@lsh.is>
2016-09-20 14:40:09 +0000

    added service and exif.js

---
+++ worker.js
@@ -0,0 +1,48 @@
+
+var _utils = require('./lib/utils.js');
+
+var configFile = __dirname + '/config.yml';
+var argv = require('yargs').argv;
+
+configFile = (typeof argv.c === 'string') ? argv.c : configFile;
+var config = _utils.readConfig(configFile);
+
+
+var Workers = require('./lib/workers.js');
+var rsmq =  _utils.rsmq(config);
+
+
+var workers = new Workers(config);
+
+var init = workers.init();
+
+var type = argv.t || process.env.TYPE || false;
+
+init.on('ready', function (){
+  var _worker = false;
+  switch (type) {
+    case 'index':
+      _worker = workers.getWorker('index');
+      break;
+    case 'sync':
+      _worker = workers.getWorker('sync');
+      break;
+    case 'thumbnail':
+      _worker = workers.getWorker('thumbnail');
+      break;
+    default:
+      _worker = false;
+  }
+
+  if (!_worker){
+    return console.log(`${type} is not a valid worker type`);
+  }
+
+  console.log(`Starting a worker, type: ${type}`);
+
+  _worker(function (err, message, next){
+    console.log(`${type} got a message...`, message.info);
+
+    setTimeout(next, 100);
+  });
+});
